284e57b7fb395ef9d15c810a30c6c36263facb35
Madda <nobody@nowhere.local>
2015-11-25 12:16:58 +0000

    Base top level URLs off common repo
    Current structure and comments suggest these are intended to all remain under one repo, but that that repo should be changed soon. This will simplify that, while also allowing removal of the NOQA.

--- get-cloudify.py
+++ get-cloudify.py
@@ -109,10 +109,12 @@ additional information.'''
 IS_VIRTUALENV = hasattr(sys, 'real_prefix')
 
 REQUIREMENT_FILE_NAMES = ['dev-requirements.txt', 'requirements.txt']
+
 # TODO: put these in a private storage
-PIP_URL = 'http://repository.cloudifysource.org/org/cloudify3/components/get-pip.py'  # NOQA
-PYCR64_URL = 'http://repository.cloudifysource.org/org/cloudify3/components/pycrypto-2.6.win-amd64-py2.7.exe'  # NOQA
-PYCR32_URL = 'http://repository.cloudifysource.org/org/cloudify3/components/pycrypto-2.6.win32-py2.7.exe'  # NOQA
+repo = 'http://repository.cloudifysource.org/org/cloudify3/components'
+PIP_URL = '{repo}/get-pip.py'.format(repo=repo)
+PYCR64_URL = '{repo}/pycrypto-2.6.win-amd64-py2.7.exe'.format(repo=repo)
+PYCR32_URL = '{repo}/pycrypto-2.6.win32-py2.7.exe'.format(repo=repo)
 
 PLATFORM = sys.platform
 IS_WIN = (PLATFORM == 'win32')
